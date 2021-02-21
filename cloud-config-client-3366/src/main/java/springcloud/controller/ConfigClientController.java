b336198a876c52e57f2c3bd1b8bbc52f075a60f1
yoscar <549953659@qq.com>
2021-02-21 18:04:18 +0800

    通过bus实现配置全局动态刷新

---
+++ cloud-config-client-3366/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    // 因为 config 仓库以 rest 形式暴露，所以所有客户端都可以通过 config 服务端访问到 github 上对应的文件信息 
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
