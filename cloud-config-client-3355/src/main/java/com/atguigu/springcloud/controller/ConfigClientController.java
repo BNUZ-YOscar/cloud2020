8db7b55cadcef56a14c5725b12b52b4f4d1b204a
yoscar <549953659@qq.com>
2021-02-21 16:28:08 +0800

    3355客户端配置中心实现手动版动态刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     // 因为 config 仓库以 rest 形式暴露，所以所有客户端都可以通过 config 服务端访问到 github 上对应的文件信息 
