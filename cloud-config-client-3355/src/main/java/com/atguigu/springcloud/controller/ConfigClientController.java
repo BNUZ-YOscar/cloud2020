8e6e7696c3efab8ba2db40943dd03b2d9f9acb80
yoscar <549953659@qq.com>
2021-02-21 16:15:13 +0800

    config配置中心客户端3344

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    // 因为 config 仓库以 rest 形式暴露，所以所有客户端都可以通过 config 服务端访问到 github 上对应的文件信息 
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
