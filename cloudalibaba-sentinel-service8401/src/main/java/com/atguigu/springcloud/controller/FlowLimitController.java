e54a11b16466a3fe05951d3d61b0e99ef9b7ca2c
yoscar <549953659@qq.com>
2021-02-25 15:53:58 +0800

    SentinelResource注解的使用

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,11 +1,16 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -16,5 +21,37 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "----testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试 RT");
+        log.info("testD 测试异常比例");
+        int age = 10 / 0;
+        return "----testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数 ");
+        int age = 10 / 0;
+        return "----testE 测试异常数 ";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "----testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "----deal_testHotKey, o(╥ ﹏ ╥)o"; // sentinel 的默认提示都是： Blocked by Sentinel (flow limiting)}
+
     }
 }
